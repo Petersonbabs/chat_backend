bcfab95f32d6fc0f6f92f664859ac497e5a97868
petersonbabs <timiebabs@gmail.com>
2025-02-19 12:58:28 +0100

    dev

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express")
 const {createServer} = require("node:http")
 const {Server} = require("socket.io")
+const cors = require("cors");
 
 const app = express()
 const server = createServer(app)
+
+app.use(cors({
+    origin: ["http://localhost:5173", "https://chat-sqi.vercel.app"],
+    methods: ["GET", "POST"]
+}));
 
 const io = new Server(server, {
     cors: {
